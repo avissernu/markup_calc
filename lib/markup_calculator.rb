dbac3041a2c78f7677a36232ea5d1633873fc123
avissernu <arnold@localhost>
2014-12-20 21:48:57 -0500

    Adding more Error conditions
    Raise exceptions when we don't have a usable product object
    change strings to symbols, believe this is better practice in ruby

--- lib/markup_calculator.rb
+++ lib/markup_calculator.rb
@@ -1,36 +1,47 @@
 class MarkupCalculator
     CATEGORY_MARKUP_RATES = Hash[
-        "pharm" => 0.075,
-        "food" => 0.13,
-        "elec" => 0.02,
-        "none" => 0
+        :pharmaceuticals => 0.075,
+        :food => 0.13,
+        :electronics => 0.02,
+        :default => 0
     ]
     BASE_MARKUP_RATE = 0.05
     PERSON_MARKUP_RATE = 0.012
     @base_price = nil
 
     def initialize product
+        if !product.respond_to?(:getPrice) || !product.respond_to?(:getCategory) || !product.respond_to?(:getPeople)
+            raise 'Product object does not implement proper interface'
+        end
         @product = product
     end
 
     def calcCost precision=2
+        @base_price = nil #clear local cache, as it's possible the price in @product changed
         retval = calcBase + calcLabourRate + calcCategoryRate
         return retval.round(precision)
     end
 
+    private
     def calcBase
-        if @base_price == nil
+        if @base_price == nil #some local caching, getPrice could be IO intensive
             orig_price = @product.getPrice
             @base_price = orig_price + (orig_price * BASE_MARKUP_RATE)
         end
         return @base_price
     end
 
+    private
     def calcLabourRate
         return (calcBase * PERSON_MARKUP_RATE) * @product.getPeople
     end
 
+    private
     def calcCategoryRate
-        return calcBase * CATEGORY_MARKUP_RATES[@product.getCategory]
+        product_category = @product.getCategory.getLabel
+        if !CATEGORY_MARKUP_RATES.include?(product_category)
+            product_category = :default
+        end
+        return calcBase * CATEGORY_MARKUP_RATES[product_category]
     end
 end
--- test/test_markup_calculator.rb
+++ test/test_markup_calculator.rb
@@ -7,27 +7,43 @@ class TestMarkupCalculator < Minitest::Test
         @product = MiniTest::Mock.new
     end
 
+    def buildMock price, people, category
+        @product.expect(:getPrice, price)
+        @product.expect(:getPeople, people)
+        category_obj = MiniTest::Mock.new
+        category_obj.expect(:getLabel, category)
+        @product.expect(:getCategory, category_obj)
+    end
+
     def test_calculator_food
-        @product.expect(:getPrice, 1299.99)
-        @product.expect(:getPeople, 3)
-        @product.expect(:getCategory, 'food')
+        buildMock 1299.99, 3, :food
         calculator = MarkupCalculator.new @product
         assert_equal 1591.58, calculator.calcCost
     end
 
     def test_calculator_pharm
-        @product.expect(:getPrice, 5432)
-        @product.expect(:getPeople, 1)
-        @product.expect(:getCategory, 'pharm')
+        buildMock 5432, 1, :pharmaceuticals
         calculator = MarkupCalculator.new @product
         assert_equal 6199.81, calculator.calcCost
     end
 
-    def test_calculator_nill
-        @product.expect(:getPrice, 12456.95)
-        @product.expect(:getPeople, 4)
-        @product.expect(:getCategory, 'none')
+    def test_calculator_books
+        buildMock 12456.95, 4, :books
         calculator = MarkupCalculator.new @product
         assert_equal 13707.63, calculator.calcCost
+    end
+
+    def test_calculator_precision
+        buildMock 999, 2, :electronics
+        calculator = MarkupCalculator.new @product
+        assert_equal 1095.104, calculator.calcCost(3)
+    end
+
+    def test_calculator_fail
+        #base mock object doesn't have the appropriate methods
+        #  setup so we can pass that and anticipate the exception
+        assert_raises (RuntimeError) do
+            calculator = MarkupCalculator.new @product
+        end
     end
 end
